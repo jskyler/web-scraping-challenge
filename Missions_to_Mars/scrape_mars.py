32affe4a6d5de35d1add44d6edee31044506126f
jskyler <72110869+jskyler@users.noreply.github.com>
2021-01-12 21:36:53 -0600

    completed python draft

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -4,32 +4,58 @@ import time
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    return Browser('chrome', **executable_path, headless=False)
 
 
 def scrape_info():
     browser = init_browser()
 
     # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
-    browser.visit(url)
+    news_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    response = requests.get(news_url)
 
     time.sleep(1)
 
     # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
+    soup = BeautifulSoup(response.text, 'html')
 
     # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    news_title = soup.find('div', class_='content_title').find('a').text
+    news_p = soup.find('div', class_='rollover_description_inner').text
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
+    
+    images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(images_url)
+    
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    main_url = 'https://www.jpl.nasa.gov'
+
+    partial_url = soup.find('article',['style']).replace('background-image: url(','').replace(');','')[1:-1]
+
+    featured_image_url = main_url+partial_url
+    
+    
+    
+    facts_url = 'https://space-facts.com/mars/'
+
+    mars_tables = pd.read_html(facts_url)
+    mars_table_df = mars_tables[0]
+    mars_table_df.set_index(0, inplace=True)
+    mars_table_df.index.names = [None]
+    mars_table_df.columns = ['']
+    
+    html_table = mars_table_df.to_html()
+    html_table = html_table.replace('\n','')
+    
+    
+    
+    
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemisphere_url)
 
     # BONUS: Find the src for the sloth image
     relative_image_path = soup.find_all('img')[2]["src"]
