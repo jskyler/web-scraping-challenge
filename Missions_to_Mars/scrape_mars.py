31d20634e4b5e763f180fcb3cbdb9e7c7d47bb9d
jskyler <72110869+jskyler@users.noreply.github.com>
2021-01-12 22:03:11 -0600

    draft scrapes complete

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,21 +1,20 @@
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import time
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
-def init_browser():
+def scrape():
+    
+    mars_dict = {}
+    
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-    return Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
+    
 
-
-def scrape_info():
-    browser = init_browser()
 
     # Visit visitcostarica.herokuapp.com
     news_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     response = requests.get(news_url)
-
-    time.sleep(1)
 
     # Scrape page into Soup
     soup = BeautifulSoup(response.text, 'html')
@@ -24,7 +23,12 @@ def scrape_info():
     news_title = soup.find('div', class_='content_title').find('a').text
     news_p = soup.find('div', class_='rollover_description_inner').text
 
+    mars_dict['news_title'] = news_title
+    mars_dict['news_p'] = news_p
 
+    
+    
+    
     
     images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(images_url)
@@ -37,6 +41,10 @@ def scrape_info():
     partial_url = soup.find('article',['style']).replace('background-image: url(','').replace(');','')[1:-1]
 
     featured_image_url = main_url+partial_url
+    
+    mars_dict['featured_image_url'] = featured_image_url
+    
+    
     
     
     
@@ -51,25 +59,40 @@ def scrape_info():
     html_table = mars_table_df.to_html()
     html_table = html_table.replace('\n','')
     
+    mars_dict['html_table'] = html_table
+    
+    
     
     
     
     hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemisphere_url)
+    
+    hemisphere_html = browser.html
+    soup = BeautifulSoup(hemisphere_html, 'html.parser')
+    
+    hemisphere_items = soup.find_all('div',class_='item')
+    
+    hemisphere_image_urls = []
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    for h in hemisphere_items:
+        title = (h.find('h3').text).replace(' Enhanced','')
 
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+        browser.click_link_by_partial_text(title)
 
-    # Close the browser after scraping
+        soup = BeautifulSoup(browser.html, 'html.parser')
+
+        full = soup.find('a', text='Sample')
+
+        img_url = full['href']
+
+        hemisphere_image_urls.append({'title': title, 'img_url': img_url})
+
+        browser.back()
+
     browser.quit()
-
-    # Return results
-    return costa_data
+    
+    mars_dict['hemisphere_image_urls'] = hemisphere_images_url
+    
+    
+    return mars_dict
