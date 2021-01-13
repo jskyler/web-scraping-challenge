9a1e815799ae3ae93bb3b86b395b5a2a7207e977
jskyler <72110869+jskyler@users.noreply.github.com>
2021-01-13 00:44:08 -0600

    Corrected comment in scrape_mars

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -13,7 +13,7 @@ def scrape():
     
 
 
-    # Visit visitcostarica.herokuapp.com
+    # Visit httpsm
     news_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     response = requests.get(news_url)
 
